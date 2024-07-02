1ea59228f9f0973851ebc29a678af66b5eee1f2b
Akshay-06 <akshayreddy1999@gmail.com>
2024-07-02 13:32:44 -0400

    Added basic code for Spring Security

---
+++ src/main/java/com/secureapp/HomeController.java
@@ -0,0 +1,14 @@
+package com.secureapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String redirect() {
+		return "home.jsp";
+	}
+
+}
